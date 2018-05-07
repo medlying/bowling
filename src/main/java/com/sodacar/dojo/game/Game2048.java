9b6c86b6a5591e0a9020049196cfcc2e748d770f
medlying <medlying@gmail.com>
2018-05-07 10:03:56 +0800

    fix

---
+++ src/main/java/com/sodacar/dojo/game/Game2048.java
@@ -0,0 +1,160 @@
+package com.sodacar.dojo.game;
+
+/**
+ * @author Hu bosong
+ * @date 2018/5/4
+ */
+public class Game2048 {
+    private int[][] checkerboard = new int[4][4];
+
+    public void init(int[][] points) {
+        checkerboard[points[0][0]][points[0][1]] = 2;
+        checkerboard[points[1][0]][points[1][1]] = 2;
+    }
+
+    public int[][] moveLeft() {
+        for (int i = 0; i < checkerboard.length; i++) {
+            int[] row = checkerboard[i];
+            int[] copy = new int[4];
+            int index = 0;
+            for (int j = 0; j < row.length; j++) {
+                if (row[j] > 0) {
+                    copy[index++] = row[j];
+                }
+            }
+            for (int j = 0; j < copy.length - 1; j++) {
+                if (copy[j] == copy[j + 1] && copy[j] > 0) {
+                    copy[j] += copy[j + 1];
+                    copy[j + 1] = 0;
+                }
+            }
+            int[] result = new int[4];
+            int resultIndex = 0;
+            for (int j = 0; j < copy.length; j++) {
+                if (copy[j] > 0) {
+                    result[resultIndex++] = copy[j];
+                }
+            }
+            for (int m = 0; m < checkerboard[i].length; m++) {
+                checkerboard[i][m] = result[m];
+            }
+        }
+        return checkerboard;
+    }
+
+    public int[][] moveRight() {
+        for (int i = 0; i < checkerboard.length; i++) {
+            int[] row = checkerboard[i];
+            int[] copy = new int[4];
+            int index = 3;
+            for (int j = row.length - 1; j >= 0; j--) {
+                if (row[j] > 0) {
+                    copy[index--] = row[j];
+                }
+            }
+            for (int j = copy.length - 1; j > 0; j--) {
+                if (copy[j] == copy[j - 1] && copy[j] > 0) {
+                    copy[j] += copy[j - 1];
+                    copy[j - 1] = 0;
+                }
+            }
+            int[] result = new int[4];
+            int resultIndex = 3;
+            for (int j = copy.length - 1; j >= 0; j--) {
+                if (copy[j] > 0) {
+                    result[resultIndex--] = copy[j];
+                }
+            }
+            for (int m = checkerboard[i].length - 1; m >= 0; m--) {
+                checkerboard[i][m] = result[m];
+            }
+        }
+        return checkerboard;
+    }
+
+    public int[][] moveUp() {
+        for (int i = 0; i < 4; i++) {
+            int[] copy = new int[4];
+            int index = 0;
+            for (int j = 0; j < checkerboard.length; j++) {
+                if(checkerboard[j][i] > 0) {
+                    copy[index++] = checkerboard[j][i];
+                }
+            }
+            for (int j = 0; j < copy.length - 1; j++) {
+                if (copy[j] == copy[j + 1] && copy[j] > 0) {
+                    copy[j] += copy[j + 1];
+                    copy[j + 1] = 0;
+                }
+            }
+            int[] result = new int[4];
+            int resultIndex = 0;
+            for (int j = 0; j < copy.length; j++) {
+                if (copy[j] > 0) {
+                    result[resultIndex++] = copy[j];
+                }
+            }
+            for (int m = 0; m < checkerboard.length; m++) {
+                checkerboard[m][i] = result[m];
+            }
+        }
+        return checkerboard;
+    }
+
+    public int[][] moveDown() {
+        for (int i = 0; i < 4; i++) {
+            int[] copy = new int[4];
+            int index = 0;
+            for (int j = checkerboard.length - 1; j >= 0; j--) {
+                if(checkerboard[j][i] > 0) {
+                    copy[index++] = checkerboard[j][i];
+                }
+            }
+            for (int j = 0; j < copy.length - 1; j++) {
+                if (copy[j] == copy[j + 1] && copy[j] > 0) {
+                    copy[j] += copy[j + 1];
+                    copy[j + 1] = 0;
+                }
+            }
+            int[] result = new int[4];
+            int resultIndex = 0;
+            for (int j = 0; j < copy.length; j++) {
+                if (copy[j] > 0) {
+                    result[resultIndex++] = copy[j];
+                }
+            }
+            for (int m = checkerboard.length - 1; m >= 0; m--) {
+                checkerboard[m][i] = result[m];
+            }
+        }
+        return checkerboard;
+    }
+
+    public void random(int[][] points, int number) {
+        checkerboard[points[0][0]][points[0][1]] = number;
+    }
+
+    public void random() {
+        int[] point;
+        do {
+            point = randomValue();
+        } while (checkerboard[point[0]][point[1]] == 0);
+        checkerboard[point[0]][point[1]] = 2;
+    }
+
+    public int[] randomValue() {
+        return new int[]{(int) Math.random() * 4, (int)Math.random() * 4};
+    }
+
+    public int getMax() {
+        int max = 0;
+        for (int i = 0; i < checkerboard.length; i++) {
+            int[] row = checkerboard[i];
+            for (int j = 0; j < row.length; j++) {
+                if (checkerboard[i][j] > max)
+                    max = checkerboard[i][j];
+            }
+        }
+        return max;
+    }
+}
---
+++ src/test/java/com/sodacar/dojo/Game2048Test.java
@@ -0,0 +1,62 @@
+package com.sodacar.dojo;
+
+import com.sodacar.dojo.game.Game2048;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Hu bosong
+ * @date 2018/5/4
+ */
+public class Game2048Test {
+
+    private Game2048 game = new Game2048();
+
+    @Test
+    public void init() {
+        game.init(new int[][]{{1, 1}, {2, 1}});
+        assertEquals(2, game.getMax());
+    }
+
+    @Test
+    public void testMoveLeft() {
+        game.init(new int[][]{{1, 1}, {2, 1}});
+        game.moveLeft();
+        assertEquals(2, game.getMax());
+    }
+
+    @Test
+    public void testMoveLeftWithHandle() {
+        game.init(new int[][]{{1, 1}, {1, 2}});
+        game.moveLeft();
+        game.random(new int[][]{{1, 3}}, 4);
+        game.moveLeft();
+        assertEquals(8, game.getMax());
+    }
+
+    @Test
+    public void testMoveLeftWithHandle1() {
+        game.init(new int[][]{{1, 1}, {1, 2}});
+        game.moveLeft();
+        game.random(new int[][]{{1, 3}}, 2);
+        game.moveLeft();
+        game.random(new int[][]{{1, 3}}, 4);
+        game.moveLeft();
+        assertEquals(4, game.getMax());
+    }
+
+    @Test
+    public void testMoveLeftWithHandle2() {
+        game.init(new int[][]{{1, 1}, {1, 2}});
+        game.moveLeft();
+        game.random(new int[][]{{1, 3}}, 2);
+        game.moveLeft();
+        game.random(new int[][]{{1, 3}}, 4);
+        game.moveLeft();
+        game.random(new int[][]{{1, 3}}, 4);
+        game.moveLeft();
+        assertEquals(8, game.getMax());
+    }
+}
